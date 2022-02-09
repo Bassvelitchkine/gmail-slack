ea46b4e18106cd7bff60f283ffe49ce52a53eda0
Bastiche <bastien.velitchkine@live.fr>
2022-02-09 21:57:17 +0100

    [+] Added global variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+function globalVariables() {
+    return Object.freeze({
+        "MAILBOX": {
+            "platform": "Platform",
+            "walb": "WALB"
+        },
+        "SPREADSHEET": {
+            "spreadsheetId": "1PV2gTej_GTE1Sa7A3-yyGNmLE-6JK3Nbev_RrRbievw",
+            "processed": {
+                "sheetName": "Processed",
+                "id": "A2:A"
+            }
+        }
+    });
+};
